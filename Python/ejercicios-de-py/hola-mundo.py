2865465383ae8522fc37f9df3fc04753606d9ea3
Tigrecito <estephanie.m.a.25@gmail.com>
2026-04-10 01:06:25 -0300

    Cambio y Core1
    Adelantamiento del core 1

---
+++ Python/ejercicios-de-py/hola-mundo.py
@@ -0,0 +1,18 @@
+#print("Hola, mundo")
+
+nombre = "estephanie"
+
+print("Hola,", nombre)
+print("Hola, "+ nombre)
+print(f"Hola, {nombre}")
+
+numero = 3
+print("Hola ", str(numero), "!")
+print("Hola " + str(numero) + "!")
+print(f"Hola {numero}!")
+
+comida_favorita1 = "milanesas"
+comida_favorita2 = "risotto"
+print("Mis comidas favoritas son las {} y el {}" .format(comida_favorita1, comida_favorita2))
+print(f"Mis comidas favoritas son las {comida_favorita1} y el {comida_favorita2}")
+print(str.upper("Mis comidas favoritas son las {} y el {}" .format(comida_favorita1, comida_favorita2)))
